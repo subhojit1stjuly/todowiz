1d89d417069e60710335461a884aecbe855e8177
Subhojit <subhojit1stjuly@gmail.com>
2024-02-29 23:07:18 +0530

    added some structure

---
+++ lib/core/utils/todo_wiz_image_assets.dart
@@ -0,0 +1,5 @@
+class TodoWizImageAssets{
+  final onboard001 = "assets/images/onboarding001.png";
+  final onboard002 = "assets/images/onboarding002.png";
+  final onboard003 = "assets/images/onboarding003.png";
+}
